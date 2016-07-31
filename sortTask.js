b4a94833783775628f25049e622e8537448a88d6
meharaj <meharaj.m@snapwiz.com>
2016-07-31 22:19:44 +0530

    added comment

--- sortTask.js
+++ sortTask.js
@@ -1,11 +1,9 @@
-var list = ['1589   5',"1003 5",' 5858 5'];
+var list = ['1589 2',"1003 1",'5858 3'];//['taskId priority', 'taskId priority']
 list = list.sort(function(task1,task2){
 	task1 = task1.trim().replace(/  +/g, ' ').split(' ');	
-	//console.log(task1);
 	var t1Id = task1[0];
 	var t1P = task1[1];
 	task2 = task2.trim().replace(/  +/g, ' ').split(' ');
-	//console.log(task2);
 	var t2Id = task2[0];
 	var t2P = task2[1];
 	var p = t1P - t2P;
@@ -15,4 +13,4 @@ list = list.sort(function(task1,task2){
 		return p;
 	}	
 });
-console.log(list.map(function(a){return a.trim().replace(/  +/g, ' ').split(' ')[0]}));
+console.log(list.map(function(a){return a.trim().replace(/  +/g, ' ').split(' ')[0]}));//showing the tasks accourding to priority or task
